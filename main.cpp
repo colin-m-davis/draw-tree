454945ef55aef9a17247ac0280494aac702196fe
colindavis <colinmichaelsdavis@gmail.com>
2023-08-10 00:51:25 -0700

    move from its to indices and fix probability bug

--- main.cpp
+++ main.cpp
@@ -17,18 +17,18 @@
 template <typename T>
 class Tree {
 public:
-  typedef std::vector<std::pair<T, unsigned>> ItemData;
   struct Node {
     unsigned weight = 0;
-    bool activated = false;
   };
+
+  using ItemData = std::vector<std::pair<T, unsigned>>;
+  using TreeIt = unsigned;
 
   unsigned numItems;
   unsigned depth;
   std::vector<Node> nodes;
   ItemData items;
-  typedef typename std::vector<Node>::iterator TreeIt;
-  TreeIt root;
+  TreeIt root = 0;
 
   // https://stackoverflow.com/questions/23781506/compile-time-computing-of-number-of-bits-needed-to-encode-n-different-states
   static constexpr unsigned floorLog2(unsigned x) {
@@ -43,52 +43,50 @@ public:
     return (2 << getDepth(numItems)) - 1;
   }
 
-  unsigned getIndex(TreeIt node) {
-    return std::distance(root, node);
-  }
-
   TreeIt getLeftChild(TreeIt parent) {
-    return std::next(root, getIndex(parent) * 2 + 1);
+    return (parent * 2) + 1;
   }
 
   TreeIt getRightChild(TreeIt parent) {
-    return std::next(getLeftChild(parent));
+    return (parent * 2) + 2;
   }
 
   TreeIt getParent(TreeIt child) {
-    return std::next(root, (getIndex(child) - 1) / 2);
+    return (child - 1) / 2;
   }
 
   unsigned fillWeights(TreeIt node) {
-    if (getIndex(node) < nodes.size() / 2) {
-      node->weight = fillWeights(getLeftChild(node)) + fillWeights(getRightChild(node));
+    if (node < nodes.size() / 2) {
+      nodes[node].weight = fillWeights(getLeftChild(node)) + fillWeights(getRightChild(node));
     }
-    return node->weight;
+    return nodes[node].weight;
   }
 
   // returns getIndex of result in leafValues
   unsigned search(TreeIt node, unsigned roll, int level) {
-    // std::cout << "search " << getIndex(node) << '\n';
+    std::cout << "search " << node << '\n';
     if (level == depth) {
-      node->activated = false;
-      return getIndex(node) - nodes.size() / 2;
+      return node - (nodes.size() / 2);
     } else {
       auto left = getLeftChild(node);
       auto right = getRightChild(node);
-      // std::cout << "generated " << roll << " out of " << left->weight << " / " << node->weight << '\n';
-      const auto result = roll <= left->weight ?
+      std::cout << "generated " << roll << " out of " << nodes[left].weight << " / " << nodes[node].weight << '\n';
+      const auto result = roll <= nodes[left].weight ?
         search(left, roll, level + 1) :
-        search(right, roll - left->weight, level + 1);
-      if (!(left->activated || right->activated)) {
-        node->activated = false;
-      }
+        search(right, roll - nodes[left].weight, level + 1);
       return result;
     }
   }
 
+  auto toggle(unsigned i, bool on = true) -> void {
+    const auto leaf = (nodes.size() / 2) + i;
+    const auto offset = (on ? 1 : -1) * static_cast<int>(items[i].second);
+    bubble(leaf, offset);
+  }
+
   void bubble(TreeIt node, int offset) {
-    node->weight += offset;
-    if (getIndex(node) > 0) {
+    nodes[node].weight += offset;
+    if (node > 0) {
       bubble(getParent(node), offset);
     }
   }
@@ -98,34 +96,26 @@ public:
     numItems(inputVec.size()),
     items(inputVec),
     depth(getDepth(inputVec.size())),
-    nodes(getNumNodes(inputVec.size()), Node()),
-    root(nodes.begin())
+    nodes(getNumNodes(inputVec.size()), Node())
   {
-    auto lastRowIt = std::next(root, nodes.size() / 2);
+    auto lastRowIt = nodes.size() / 2;
     for (auto inputIt = inputVec.cbegin(); inputIt < inputVec.cend(); ++inputIt) {
-      *(lastRowIt++) = Node{inputIt->second, true};
-    }
-    for (; lastRowIt < nodes.end(); ++lastRowIt) {
-      *lastRowIt = Node{};
+      nodes[lastRowIt++].weight = inputIt->second;
     }
     fillWeights(root);
   }
 
   std::mt19937 gen32{std::random_device()()};
 
-  void toggle(unsigned i, bool on = true) {
-    const auto leaf = nodes.begin() + (nodes.size() / 2) + i;
-    const auto offset = (on ? 1 : -1) * static_cast<int>(items[i].second);
-    bubble(leaf, offset);
-  }
-
-  std::vector<T> get(unsigned count) {
+  auto get(unsigned count) -> std::vector<T> {
     std::vector<unsigned> indices;
     indices.reserve(count);
     for (int iterations = 0; iterations < count; ++iterations) {
-      std::uniform_int_distribution<unsigned> dist(0, root->weight);
+      std::uniform_int_distribution<unsigned> dist(1, nodes[root].weight);
       auto roll = dist(gen32);
+      std::cout << "getting next value ! ! !" << '\n';
       auto i = search(root, roll, 0);
+      std::cout << "got " << i << "(" << items[i].first << ")" << '\n';
       toggle(i, false);
       indices.push_back(i);
     }
@@ -139,11 +129,6 @@ public:
   }
 };
 
-uint64_t timeSinceEpochMillisec() {
-  using namespace std::chrono;
-  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-}
-
 template<typename T>
 void print(std::vector<T> inputVec) {
   for (const auto& x : inputVec) {
@@ -151,6 +136,12 @@ void print(std::vector<T> inputVec) {
   }
   std::cout << '\n';
 }
+
+uint64_t timeSinceEpochMillisec() {
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
 
 uint64_t measure(const std::function<void()>& f) {
   auto start = timeSinceEpochMillisec();
@@ -161,11 +152,13 @@ uint64_t measure(const std::function<void()>& f) {
 
 int main() {
   std::vector<std::pair<int, unsigned>> inputVec;
-  for (int i = 0; i < 10125; ++i) {
+  for (int i = 1; i < 11; ++i) {
     inputVec.emplace_back(i, i);
   }
   Tree t(inputVec);
-  std::cout << measure([&t]() { t.get(10000); }) << '\n';
+  auto res = t.get(10);
+  print(res);
+  // std::cout << measure([&t]() { t.get(1000); }) << '\n';
   // std::cout << t.search << '\n';
   return 0;
 }
